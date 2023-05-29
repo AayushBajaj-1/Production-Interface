7cae643b61abead93858f6adf895fb47e959e674
Aayush <aayush.bajaj@vention.cc>
2023-05-29 17:28:42 -0400

    working socket client

--- server/testing.js
+++ server/testing.js
@@ -1,8 +1,8 @@
 const { Client } = require("ssh2");
-const { spawn } = require("node-pty");
 const readline = require("readline");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const express = require("express");
-const cors = require("cors");
 
 const scriptPath =
     "/var/lib/cloud9/vention-control/tests/production_qa_scripts/util/autoSetup.py";
@@ -20,78 +20,99 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-// Start a ssh client
-const ssh = new Client();
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3001", // Replace with the URL of your React app
+        methods: ["GET", "POST"],
+    },
+});
+
 let sshStream;
 
-const app = express();
-app.use(cors());
+io.on("connection", (socket) => {
+    console.log("Client connected");
+    // Start a ssh client
+    const ssh = new Client();
 
-ssh.on("ready", () => {
-    console.log("SSH connection established");
-    ssh.shell((err, stream) => {
-        if (err) throw err;
+    ssh.on("ready", () => {
+        console.log("SSH connection established");
+        ssh.shell((err, stream) => {
+            if (err) {
+                ssh.end(); // Close the SSH connection
+                socket.emit("error", "Error connecting to SSH");
+                socket.disconnect();
+                return;
+            }
 
-        sshStream = stream;
-        // Handle data received from the SSH shell
-        stream.on("data", (data) => {
-            // If it is an input then ask for the input and write it to the shell
-            if (
-                data.indexOf("Input:") !== -1 ||
-                data.indexOf("password for debian:") !== -1
-            ) {
-                // If the prompt is detected, write the input
-                // Print data received from the SSH shell
-                console.log(`${data}`);
-                promptInput();
-            } else {
-                // Print data received from the SSH shell
-                console.log(`${data}`);
+            sshStream = stream;
+            // Handle data received from the SSH shell
+            stream.on("data", (data) => {
+                // If it is an input then ask for the input and write it to the shell
+                if (
+                    data.indexOf("Input:") !== -1 ||
+                    data.indexOf("password for debian:") !== -1
+                ) {
+                    // If the prompt is detected, write the input
+                    // Print data received from the SSH shell
+                    console.log(`${data}`);
+                    socket.emit("input", data);
+                    promptInput();
+                } else {
+                    // Print data received from the SSH shell
+                    console.log(`${data}`);
+                    socket.emit("output", data);
+                }
+            });
+
+            // Handle SSH shell close event
+            stream.on("close", () => {
+                console.log("SSH shell closed");
+                ssh.end(); // Close the SSH connection
+                socket.emit("error", "Script execution incomplete");
+                socket.disconnect();
+            });
+
+            // Prompt for input and pass it to the SSH shell
+            function promptInput() {
+                rl.question("", (input) => {
+                    stream.write(`${input}\n`);
+                    promptInput();
+                });
             }
         });
+    });
 
-        // Handle SSH shell close event
-        stream.on("close", () => {
-            console.log("SSH shell closed");
-            ssh.end(); // Close the SSH connection
-            //process.exit(); // Exit the Node.js process
-        });
+    socket.on("start", () => {
+        console.log("Starting SSH connection...");
+        ssh.connect(sshConfig);
+    });
 
-        // Prompt for input and pass it to the SSH shell
-        function promptInput() {
-            rl.question("", (input) => {
-                stream.write(`${input}\n`);
-                promptInput();
-            });
-        }
+    socket.on("runSetup", () => {
+        console.log("Running setup script...");
+        sshStream.write(`sudo python3 ${scriptPath} auto\n`);
+        sshStream.write(`temppwd\n`);
+
+        setTimeout(() => {
+            sshStream.write(`\n`);
+        }, 3000);
+    });
+
+    socket.on("close", () => {
+        sshStream.end();
+        ssh.end();
+        socket.disconnect();
+    });
+
+    // Socket disconnect event
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
     });
 });
 
-app.get("/client/run", (req, res) => {
-    sshStream.write(`sudo python3 ${scriptPath} auto\n`);
-    sshStream.write(`temppwd\n`);
-    setTimeout(() => {
-        sshStream.write(`\n`);
-    }, 3000);
-    res.send(`Running the script in the background`);
-});
-
-app.get("/client/close", (req, res) => {
-    ssh.end(); // Close the SSH connection
-    res.send({
-        message: "Successfully closed the connection",
-    });
-});
-
-app.get("/client/open", (req, res) => {
-    ssh.connect(sshConfig); // Close the SSH connection
-    setTimeout(() => {
-        res.send({
-            message: "Successfully opened the connection",
-        });
-    }, 2000);
-});
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+// Start the HTTP server
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
